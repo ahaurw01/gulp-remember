7285af64200a4bd8de3a361d15e3b01f0d69b7b6
ahaurw01 <aaron.haurwitz@gmail.com>
2014-12-20 09:01:35 -0500

    add a forgetAll feature

--- index.js
+++ index.js
@@ -65,4 +65,24 @@ gulpRemember.forget = function (cacheName, path) {
   }
 };
 
+/**
+ * Forget all files in one cache.
+ * A warning is logged if the cache does not exist.
+ *
+ * @param cacheName {string} name of the cache to wipe
+ */
+gulpRemember.forgetAll = function (cacheName) {
+  if (arguments.length === 0) {
+    cacheName = defaultName;
+  }
+  if (typeof cacheName !== 'number' && typeof cacheName !== 'string') {
+    throw new util.PluginError(pluginName, 'Usage: require("gulp-remember").forgetAll(cacheName); where cacheName is undefined, number or string');
+  }
+  if (caches[cacheName] === undefined) {
+    util.log(pluginName, '- .forget() warning: cache ' + cacheName + ' not found');
+  } else {
+    caches[cacheName] = {};
+  }
+}
+
 module.exports = gulpRemember;
